e489452090b195e04c10ae21c81f2410cabcec02
Alexander Bondaletov <alexander.bondaletov@redfield.se>
2020-07-10 11:03:45 +0300

    Project setup

---
+++ se.redfield.bert/src/se/redfield/bert/nodes/BertTokenizerNodeModel.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2020 Redfield AB.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, Version 3, as
+ * published by the Free Software Foundation.
+ *  
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses>.
+ */
+package se.redfield.bert.nodes;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.knime.core.data.DataTableSpec;
+import org.knime.core.node.BufferedDataTable;
+import org.knime.core.node.CanceledExecutionException;
+import org.knime.core.node.ExecutionContext;
+import org.knime.core.node.ExecutionMonitor;
+import org.knime.core.node.InvalidSettingsException;
+import org.knime.core.node.NodeModel;
+import org.knime.core.node.NodeSettingsRO;
+import org.knime.core.node.NodeSettingsWO;
+
+public class BertTokenizerNodeModel extends NodeModel {
+
+	protected BertTokenizerNodeModel() {
+		super(0, 0);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected BufferedDataTable[] execute(BufferedDataTable[] inData, ExecutionContext exec) throws Exception {
+		// TODO Auto-generated method stub
+		return super.execute(inData, exec);
+	}
+
+	@Override
+	protected DataTableSpec[] configure(DataTableSpec[] inSpecs) throws InvalidSettingsException {
+		// TODO Auto-generated method stub
+		return super.configure(inSpecs);
+	}
+
+	@Override
+	protected void loadInternals(File nodeInternDir, ExecutionMonitor exec)
+			throws IOException, CanceledExecutionException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void saveInternals(File nodeInternDir, ExecutionMonitor exec)
+			throws IOException, CanceledExecutionException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void saveSettingsTo(NodeSettingsWO settings) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void validateSettings(NodeSettingsRO settings) throws InvalidSettingsException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void loadValidatedSettingsFrom(NodeSettingsRO settings) throws InvalidSettingsException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void reset() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
