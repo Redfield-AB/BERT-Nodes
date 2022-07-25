4307560cf9479e486f0a822c11c06280c904a524
Adrian Nembach <adrian.nembach@knime.com>
2022-07-25 11:22:44 +0200

    Use document text instead of document title

--- se.redfield.bert/src/se/redfield/bert/util/InputUtils.java
+++ se.redfield.bert/src/se/redfield/bert/util/InputUtils.java
@@ -15,12 +15,18 @@
  */
 package se.redfield.bert.util;
 
+import java.util.Optional;
+import java.util.function.Function;
+
 import org.knime.core.data.DataCell;
 import org.knime.core.data.DataColumnSpecCreator;
 import org.knime.core.data.DataRow;
+import org.knime.core.data.DataType;
+import org.knime.core.data.StringValue;
 import org.knime.core.data.container.ColumnRearranger;
 import org.knime.core.data.container.SingleCellFactory;
 import org.knime.core.data.def.StringCell;
+import org.knime.ext.textprocessing.data.DocumentValue;
 
 /**
  * Provides utilities to prepare the input table for Python.
@@ -28,40 +34,64 @@ import org.knime.core.data.def.StringCell;
  * @author Adrian Nembach, KNIME GmbH, Konstanz, Germany
  */
 public final class InputUtils {
-	
+
 	private InputUtils() {
-		
+
 	}
-	
+
 	/**
-	 * Converts the provided columns to string columns if they aren't already string columns.
+	 * Converts the provided columns to string columns if they aren't already string
+	 * columns.
 	 * 
 	 * @param rearranger the rearranger to modify
-	 * @param columns name of the columns to convert
+	 * @param columns    name of the columns to convert
 	 */
-	public static void convertColumnsToString(final ColumnRearranger rearranger, final String ...columns) {
+	public static void convertColumnsToString(final ColumnRearranger rearranger, final String... columns) {
 		var spec = rearranger.createSpec();
 		for (var column : columns) {
 			var colIdx = spec.findColumnIndex(column);
 			var colSpec = spec.getColumnSpec(colIdx);
-			if (!colSpec.getType().equals(StringCell.TYPE)) {
-				rearranger.replace(new ToStringCellFactory(colIdx, colSpec.getName()), colIdx);
-			}
+			getConverter(colSpec.getType())//
+					.ifPresent(c -> rearranger.replace(
+							new ToStringCellFactory(colIdx, colSpec.getName(), InputUtils::stringValueToString),
+							colIdx));
 		}
 	}
 	
+	private static Optional<Function<DataCell, String>> getConverter(DataType type) {
+		if (type.isCompatible(DocumentValue.class)) {
+			return Optional.of(InputUtils::documentValueToString);
+		} else if (!type.equals(StringCell.TYPE)) {
+			return Optional.of(InputUtils::stringValueToString);
+		} else {
+			return Optional.empty();
+		}
+	}
+	
+	private static String documentValueToString(final DataCell cell) {
+		var document = ((DocumentValue)cell).getDocument();
+		return document.getDocumentBodyText();
+	}
+
+	private static String stringValueToString(final DataCell cell) {
+		return ((StringValue) cell).getStringValue();
+	}
+
 	/**
 	 * Turns any column into a String column.
 	 * 
 	 * @author Adrian Nembach, KNIME GmbH, Konstanz, Germany
 	 */
-	private static class ToStringCellFactory extends SingleCellFactory {
+	private static final class ToStringCellFactory extends SingleCellFactory {
 
 		private final int m_colIdx;
-		
-		ToStringCellFactory(int colIdx, String name) {
+
+		private final Function<DataCell, String> m_converter;
+
+		ToStringCellFactory(int colIdx, String name, final Function<DataCell, String> converter) {
 			super(new DataColumnSpecCreator(name, StringCell.TYPE).createSpec());
 			m_colIdx = colIdx;
+			m_converter = converter;
 		}
 
 		@Override
@@ -70,10 +100,10 @@ public final class InputUtils {
 			if (cell.isMissing()) {
 				return cell;
 			} else {
-				return new StringCell(cell.toString());
+				return new StringCell(m_converter.apply(cell));
 			}
 		}
-		
+
 	}
 
 }
